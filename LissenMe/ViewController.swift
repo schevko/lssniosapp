40688e8da29b7158afee4a634bf42385e4563af8
Şevket Erer <sevketerer@sevkets-mac.local>
2018-02-28 01:56:32 -0800

    Initial Commit

---
+++ LissenMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LissenMe
+//
+//  Created by Şevket Erer on 28.02.2018.
+//  Copyright © 2018 Şevket Erer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
